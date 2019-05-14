1880d914ee7f8667a019d8d1a2bc2600e7c2d7af
deehgaspar <andregaspar_@hotmail.com>
2019-05-14 16:49:42 -0300

    adicionando 4 e 5

---
+++ Exercicio 4/app/src/main/java/br/usjt/ciclodevidagpsemapas/ListaLocaisActivity.java
@@ -0,0 +1,48 @@
+/**
+ * Daniel Moudatsos Strassacapa Rodrigues R.A 81716932
+ */
+
+package br.usjt.ciclodevidagpsemapas;
+
+import android.content.Intent;
+import android.location.Location;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ListaLocaisActivity extends AppCompatActivity {
+
+    private ListView locationsListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_locais);
+        Intent origemIntent = getIntent();
+        final ArrayList<Localizacao> locations = (ArrayList<Localizacao>) origemIntent.getSerializableExtra("locations");
+        locationsListView = findViewById(R.id.localizacoesListView);
+        LocalizacoesArrayAdapter adapter = new LocalizacoesArrayAdapter(this, locations);
+        locationsListView.setAdapter(adapter);
+
+        locationsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Localizacao location = locations.get(position);
+
+                Uri gmmIntentUri = Uri.parse(String.format("geo:%f,%f", location.getLongitude(), location.getLatitude()));
+
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+
+
+    }
+}
---
+++ Exercicio 5/app/src/main/java/br/usjt/ciclodevidagpsemapas/MainActivity.java
@@ -0,0 +1,148 @@
+/**
+ * Daniel Moudatsos Strassacapa Rodrigues R.A 81716932
+ */
+
+package br.usjt.ciclodevidagpsemapas;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView locationTextView;
+
+    public static final int REQUEST_CODE_GPS = 1001;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    private double latitudeAtual;
+    private double longitudeAtual;
+    private ArrayList<Localizacao> locations;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        locations = new ArrayList<>();
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListaLocaisActivity.class);
+                intent.putExtra("locations", locations);
+                startActivity(intent);
+            }
+        });
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitudeAtual = location.getLatitude();
+                longitudeAtual = location.getLongitude();
+                Localizacao localizacao = new Localizacao();
+                localizacao.setLatitude(latitudeAtual);
+                localizacao.setLongitude(longitudeAtual);
+                locations.add(localizacao);
+                if(locations.size() > 50){
+                    Log.e("Size",">50");
+                    locations.remove(0);
+                }
+                locationTextView.setText(String.format("Lat: %f, Long: %f", latitudeAtual, longitudeAtual));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        locationTextView = findViewById(R.id.locationTextView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, APPConstants.REQUEST_LOCATION_UPDATE_MIN_TIME, APPConstants.REQUEST_LOCATION_UPDATE_MIN_DISTANCE, locationListener);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_GPS);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_GPS) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, APPConstants.REQUEST_LOCATION_UPDATE_MIN_TIME, APPConstants.REQUEST_LOCATION_UPDATE_MIN_DISTANCE, locationListener);
+                }
+            } else {
+                Toast.makeText(this, getString(R.string.no_gps_no_app), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
